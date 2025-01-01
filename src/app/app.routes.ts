b53c54f155e551e8e8f1f1215d1f5fa1ed3581db
Tien Kim <tienkim9920@gmail.com>
2025-01-01 10:55:40 +0700

    update\

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,16 @@ import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'detail/:id', component: DetailComponent },
-  { path: 'create', component: CreateComponent },
-  // {
-  //   path: 'detail/:id',
-  //   loadComponent: () =>
-  //     import('./detail/detail.component').then((m) => m.DetailComponent),
-  // },
-  // {
-  //   path: 'create',
-  //   loadComponent: () =>
-  //     import('./create/create.component').then((m) => m.CreateComponent),
-  // },
+  // { path: 'detail/:id', component: DetailComponent },
+  // { path: 'create', component: CreateComponent },
+  {
+    path: 'detail/:id',
+    loadComponent: () =>
+      import('./detail/detail.component').then((m) => m.DetailComponent),
+  },
+  {
+    path: 'create',
+    loadComponent: () =>
+      import('./create/create.component').then((m) => m.CreateComponent),
+  },
 ];
